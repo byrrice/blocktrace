8a9359d8a02574a90302ac972bceccfaa606bf1e
Isaac Ng <isaacng@Isaacs-MacBook-Pro.local>
2023-02-28 16:32:56 -0600

    Finished table and bar chart

--- client/src/components/FormComponent.js
+++ client/src/components/FormComponent.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import './FormComponent.css';
+import Chart from "react-apexcharts";
 
 function FormComponent() {
 
@@ -8,6 +9,7 @@ function FormComponent() {
   const [apiKey, setApiKey] = useState("");
   const [cryptoAddressType, setCryptoAddressType] = useState("");
   const [message, setMessage] = useState("");
+  const [tableData, setTableData] = useState(null);
 
   let handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,25 +23,29 @@ function FormComponent() {
       //       addresses: [
       //         "36azMEqtTmt2m5PJb4QmWydL8kL1HPHs8d",
       //         "35EHWhDeYrYkMgq8e8wBBnXbEUVNaw5kys",
-      //         "3NV7CWkxKmf997KfDKNxS9by8mV6bxh4sn"
+      //         "3NV7CWkxKmf997KfDKNxS9by8mV6bxh4sn",
+      //         "bc1qu0rqae6402ds038r7m6cjqqyctl4ussjc6wy474cxgl3t6utrktqvwkat4",
+      //         "bc1qm8d48c6qx59umf8euk2ejnslt7gr5f6ym4mxrs06fyg647msg67ql72d8l",
+      //         "bc1q7fa99vygj69cegdp6d0qnkjpkmnu5ayv22dn8cxj0xldke0ltstsz5ru27"
       //       ],
       //       apiKey: 'BT-9484d2ac-2056-4c9b-aadd-828bb34a3317'
       //     }
       //   })
       let res = await axios.post("/blocktrace",
-      {},
-      {
-        params: {
-          type: cryptoAddressType,
-          addresses: cryptoAddress.split(/\s+/),
-          apiKey: apiKey
-        }
-      })
+        {},
+        {
+          params: {
+            type: cryptoAddressType,
+            addresses: cryptoAddress.split(/\s+/),
+            apiKey: apiKey
+          }
+        })
       console.log(res.data);
       if (res.status === 200) {
         // setCryptoAddress("");
         // setApiKey("");
-        setMessage("Data Successfully Fetched");
+        setTableData(res.data)
+        setMessage("Data Retrieved!");
       }
     } catch (err) {
       setMessage(err.response.status + " " + err.response.data.message);
@@ -73,6 +79,55 @@ function FormComponent() {
 
         <div className="message">{message ? <p>{message}</p> : null}</div>
       </form>
+
+      {tableData !== null && <div className="visualizeData"><table>
+        <tr>
+          <th>Address</th>
+          <th>Address Name</th>
+          <th>Address Type</th>
+          <th>Balance</th>
+          <th>Total Sent</th>
+          <th>Total Received</th>
+          <th>Risk Score</th>
+        </tr>
+        {tableData.map(({ summary: { address, name, type, balance, totalSentAmount, totalReceivedAmount, score } }, key) => {
+          return (
+            <tr key={key}>
+              <td>{address}</td>
+              <td>{name}</td>
+              <td>{type}</td>
+              <td>{balance}</td>
+              <td>{totalSentAmount}</td>
+              <td>{totalReceivedAmount}</td>
+              <td>{score}</td>
+            </tr>
+          )
+        })}
+      </table>
+        <Chart
+          options={{
+            chart: {
+              id: "basic-bar"
+            },
+            xaxis: {
+              categories: tableData.length >= 5
+                ? tableData.sort((a, b) => b.summary.totalSentAmount - a.summary.totalSentAmount).slice(0, 5).map(function (item) { return item["summary"]["address"]; })
+                : tableData.sort((a, b) => b.summary.totalSentAmount - a.summary.totalSentAmount).slice(0, tableData.length).map(function (item) { return item["summary"]["address"]; })
+            }
+          }}
+          series={[
+            {
+              name: "series-1",
+              data: tableData.length >= 5
+                ? tableData.sort((a, b) => b.summary.totalSentAmount - a.summary.totalSentAmount).slice(0, 5).map(function (item) { return item["summary"]["totalSentAmount"]; })
+                : tableData.sort((a, b) => b.summary.totalSentAmount - a.summary.totalSentAmount).slice(0, tableData.length).map(function (item) { return item["summary"]["totalSentAmount"]; })
+            }
+          ]}
+          type="bar"
+          width="1000"
+        />
+      </div>
+      }
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -5,6 +5,13 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
